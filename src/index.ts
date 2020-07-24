ba962b181029d26e9905a0347aa44a3f768468ab
Andrew Mclagan <andrewmclagan@gmail.com>
2020-07-24 14:21:10 +1000

    test coverage

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,28 @@ import Cmd from "./Cmd.ts";
 import Plugin from "./Plugin.ts";
 
 let glob = Env.get(["GKE_GLOB", "PLUGIN_GLOB"]);
-let repository = Env.get(["GKE_REPOSITORY", "PLUGIN_REPOSITORY"]);
 let cluster = Env.get(["GKE_CLUSTER", "PLUGIN_CLUSTER"]);
 
 let config = {
   glob,
-  repository: repository ?? {
-    ...repository,
-    netrc: {
-      machine: Env.get('DRONE_NETRC_MACHINE'),
-      login: Env.get('DRONE_NETRC_USERNAME'),
-      password: Env.get('DRONE_NETRC_PASSWORD')
-    }
-  },
+  repository: undefined,
   cluster: {
     ...cluster,
     serviceKey: jsonParse(base64Decode(cluster.serviceKey)),
   },
 };
+
+let repository = Env.get(["GKE_REPOSITORY", "PLUGIN_REPOSITORY"]);
+
+if (repository) {
+  config.repository = {
+    ...repository,
+    netrc: {
+      machine: Env.get("DRONE_NETRC_MACHINE"),
+      login: Env.get("DRONE_NETRC_USERNAME"),
+      password: Env.get("DRONE_NETRC_PASSWORD"),
+    },
+  };
+}
 
 new Plugin(config, new Cmd()).run();
