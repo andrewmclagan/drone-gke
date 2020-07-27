7eb86527cfbaa0e11074ce68c8ad40afa8a5d54e
Andrew Mclagan <andrewmclagan@gmail.com>
2020-07-27 15:55:32 +1000

    invalid gc key format

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,14 @@ import Plugin from "./Plugin.ts";
 let glob = Env.get(["GKE_GLOB", "PLUGIN_GLOB"]);
 let repository = Env.get(["GKE_REPOSITORY", "PLUGIN_REPOSITORY"]);
 let cluster = Env.get(["GKE_CLUSTER", "PLUGIN_CLUSTER"]);
+let serviceKey = Env.get(["GKE_SERVICE_KEY", "PLUGIN_SERVICE_KEY"]);
 
 let config = {
   glob,
   repository: undefined,
   cluster: {
     ...cluster,
-    service_key: jsonParse(base64Decode(cluster.service_key)),
+    service_key: jsonParse(base64Decode(serviceKey)),
   },
 };
 
